b077cac29ceb91271ca76f29cc3e892593fcffa0
jgillick <j_gillick@yahoo.com>
2015-11-03 10:02:30 -0800

    Refine example. Add doc.

--- Serial/example/main.cpp
+++ Serial/example/main.cpp
@@ -1,13 +1,28 @@
+/*#####################################
+ *
+ * Echos whatever is received on RX to TX.
+ * If you connect an LED to PD5, it will
+ * light up as soon as data has been
+ * received.
+ *
+ #####################################*/
+
 #include <avr/io.h>
 
 #include "../Serial.h"
+
+#define LED PD5
 
 int main(void){
 
   Serial.begin(9600);
 
+  DDRD  |= (1 << LED);
+
   while(1) {
     if (Serial.available()) {
+      PORTD |= (1 << LED);
+
       Serial.print("\nReceived: ");
       Serial.write(Serial.read());
       Serial.print("\n");
